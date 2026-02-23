a70786e2e8439bba6080b56b6c11632a6f6b8259
Satyanchal Chaudhary <62104921+iamsatyanchal@users.noreply.github.com>
2026-02-23 23:42:10 +0530

    Enhance epoxy.js with version and help options
    Added version and help commands to epoxy script.

--- bin/epoxy.js
+++ bin/epoxy.js
@@ -5,6 +5,16 @@ import path from "path";
 import { run } from "../src/index.js";
 
 const file = process.argv[2];
+if (process.argv[2] === "--version") {
+    console.log("Epoxy version 1.0.0");
+    process.exit(0);
+}
+
+if (process.argv[2] === "--help") {
+    console.log("Epoxy version 1.0.0");
+    console.log("Usage: epoxy <file.epx>");
+    process.exit(0);
+}
 
 if (!file) {
     console.error("Usage: epoxy <file.epx>");
@@ -15,6 +25,7 @@ if (!file.endsWith(".epx")) {
     console.error("Only .epx files allowed");
     process.exit(1);
 }
+
 
 const code = fs.readFileSync(path.resolve(file), "utf8");
 run(code);
